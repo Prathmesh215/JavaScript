a4f83119247b89624ac112aedd03e1d666311c9e
Prathmesh215 <125857624+Prathmesh215@users.noreply.github.com>
2024-11-30 20:29:15 +0530

    Add files via upload

---
+++ JsCode/Check_list.js
@@ -0,0 +1,9 @@
+var name = [1,2,3,4,5];
+
+var guestName = prompt("What is your name ?");
+
+if(name.includes(guestName)){
+    alert("Welcome.");
+} else {
+    alert("Sorry, me be next time.");
+}
---
+++ JsCode/DogtoHumanage.js
@@ -0,0 +1,7 @@
+//Dog age to human Age calculater
+
+var dogAge = prompt("How old is your dog ?");
+
+var toHumanAge = (dogAge - 2) * 4 + 21;
+
+alert("Your dog is " + toHumanAge + " years old in human years.");
